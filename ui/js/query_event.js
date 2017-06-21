5f77b4337ff63baa3feda1098c12bdb966e7fc96
Wendy Hu <whu@wish.com>
2017-06-20 17:47:54 -0700

    Update glide deps, use dropdowns for sort

--- ui/js/query_event.js
+++ ui/js/query_event.js
@@ -1,4 +1,6 @@
 var params = [];
+var sortFields = 0;
+var querySuccess = true;
 
 function updateResults() {
 	$.ajax({
@@ -6,6 +8,7 @@ function updateResults() {
 		url: "/v1/event?"+params.join("&"),
 		dataType: "json",
 		success: function(data) {
+            querySuccess = true;
 			var elem = document.getElementById("event_table")
 			elem.innerHTML = "";
 			var results = data["results"];
@@ -30,13 +33,14 @@ function updateResults() {
 			}
 		},
 		error: function(data) {
+            querySuccess = false;
 			alert("Error querying events: " + data.responseText);
 		}
 	});
 }
 
 function backgroundUpdate() {
-	if (document.getElementById("refreshCheckbox").checked) {
+	if (querySuccess && document.getElementById("refreshCheckbox").checked) {
 		updateResults()
 	}
 	setTimeout(backgroundUpdate, 5000)
@@ -52,36 +56,43 @@ $(document).ready(function() {
 function submitQuery(form) {
 	var data = $(form).serializeArray();
 	var formData = {};
+    var sortFields = [];
+    var sortAscending = [];
 	var startEventTime, endEventTime;
 	for (var i = 0; i < data.length; i++) {
 		var key = data[i]["name"];
 		var value = data[i]["value"];
 		if (value) {
-			switch(key) {
-				case "sort_ascending":
-					var sortArr = value === "" ? [] : value.split(",")
-					sortArr.map(function(v) {
-						if (v.toLowerCase() === "t" || v.toLowerCase() === "true") {
-							return true;
-						}
-						return false;
-					})
-					formData[key] = sortArr;
-					break;
-				case "data":
-					formData[key] = value
-					break;
-				case "startEventTime":
-					startEventTime = value;
-					break;
-				case "endEventTime":
-					endEventTime = value;
-					break;
-				default:
-					formData[key] = value === "" ? [] : value.split(",");
+            if (key.startsWith("sort_field")) {
+                sortFields.push(value);
+            } else if (key.startsWith("sort_ascending")) {
+                if (value.toLowerCase() === "t" || value.toLowerCase() === "true") {
+                    sortAscending.push("true")
+                } else {
+                    sortAscending.push("false")
+                }
+            } else {
+                switch(key) {
+				    case "data":
+					    formData[key] = value
+					    break;
+				    case "startEventTime":
+					    startEventTime = value;
+					    break;
+				    case "endEventTime":
+					    endEventTime = value;
+					    break;
+				    default:
+				    	formData[key] = value === "" ? [] : value.split(",");
+                }
 			}
 		}
 	}
+
+    if (sortFields.length > 0) {
+        formData["sort_field"] = sortFields;
+        formData["sort_ascending"] = sortAscending;
+    }
 
 	if (startEventTime) {
 	    formData["start_event_time"] = getTimestamp(startEventTime);
@@ -103,4 +114,26 @@ function submitQuery(form) {
 	}
 	updateResults()
 	return false;
+}
+
+function addSortField() {
+    var newSortField = document.createElement('div');
+    var html = `<div class="col-sm-6"><select name="sort_field` + sortFields + `" class="form-control" style="display: inline-block;">
+            <option value=""></option>
+            <option value="topic">topic</option>
+            <option value="dc">dc</option>
+            <option value="host">host</option>
+            <option value="target_host_set">target host set</option>
+            <option value="user">user</option>
+            <option value="tag_set">tag</option>
+            <option value="parent_event_id">parent event id</option>
+            <option value="event_time">event time</option>
+        </select></div>
+        <div class="col-sm-6"><select name="sort_ascending` + sortFields + `" class="form-control" style="display: inline-block;">
+            <option value="true">ascending</option>
+            <option value="false">descending</option>
+        </select></div>`;
+    newSortField.innerHTML = html;
+    sortFields++;
+    document.getElementById("sortFields").appendChild(newSortField);
 }
