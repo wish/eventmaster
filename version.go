d7161a2766420f5f7bfd5fff120ab97633ff0184
Stephen McQuay <smcquay@wish.com>
2017-09-28 15:41:53 -0700

    expose version over http

--- version.go
+++ version.go
@@ -1,4 +1,26 @@
 package eventmaster
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
 var Version = "unset"
 var Git = "unset"
+
+func (h *Server) version(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	r := struct {
+		Version string `json:"version"`
+		Git     string `json:"git"`
+	}{
+		Version: Version,
+		Git:     Git,
+	}
+	if err := json.NewEncoder(w).Encode(&r); err != nil {
+		log.Printf("json encode: %+v", err)
+	}
+}
